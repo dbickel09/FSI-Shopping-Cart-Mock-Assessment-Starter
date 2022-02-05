693d6f5b59a599879e7c3ca9d1f7b2f9df6fa4f9
Dan <bickel09@yahoo.com>
2022-02-05 12:10:50 -0600

    Step Four Done

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-//Plus and minus buttons respond to a click event
-//Plus button adds one to the product quantity
-//Minus button subtracts one from the product quantity
-//Quantity updates when the plus or minus buttons are clicked
+//*Plus and minus buttons respond to a click event
+//*Plus button adds one to the product quantity
+//*Minus button subtracts one from the product quantity
+//*Quantity updates when the plus or minus buttons are clicked
 //Quantity cannot drop below zero
 
 let rockQty = 1
@@ -9,14 +9,15 @@ let rockQty = 1
 // access the buttons
 let addBtn = document.querySelector('#quantity-up')
 let minusBtn = document.querySelector('#quantity-down')
+let qtyDisplay = document.querySelector('.total-quantity')
 
 // add a click event to each button
 addBtn.addEventListener('click', function(e) {
     rockQty++
-    console.log(rockQty)
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
 })
 
 minusBtn.addEventListener('click', function(e) {
     rockQty--
-    console.log(rockQty)
+    qtyDisplay.textContent = `Quantity: ${rockQty}`
 })
